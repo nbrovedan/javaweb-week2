74e4e9a4efa81a9ac25f78296925c91ef1e849e7
Vinicius Rodrigues Nunes <vinny@voffice.com.br>
2018-09-21 23:50:49 -0300

    Disable cors

--- src/main/java/br/com/voffice/java/jwptf02/week2/application/controllers/CorsFilter.java
+++ src/main/java/br/com/voffice/java/jwptf02/week2/application/controllers/CorsFilter.java
@@ -7,11 +7,9 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebFilter(urlPatterns = { "/*" })
 public class CorsFilter implements Filter {
 
 	@Override
